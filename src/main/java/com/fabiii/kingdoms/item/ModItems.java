3c49bd52ace1de46c7e87c8563e63ec19224ab5b
FabianEll <103959286+x75b24@users.noreply.github.com>
2022-09-21 21:59:52 +0200

    added textures for crown and coin, changed mappings to parchment 1.18.1-2022.03.06-1.18.2

---
+++ src/main/java/com/fabiii/kingdoms/item/ModItems.java
@@ -0,0 +1,10 @@
+package com.fabiii.kingdoms.item;
+
+import com.fabiii.kingdoms.Kingdoms;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Kingdoms.MOD_ID);
+}
