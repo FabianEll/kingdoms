ba3f414f76c70282efafe1cf325fcbb351c759a9
FabianEll <103959286+x75b24@users.noreply.github.com>
2022-09-22 15:10:29 +0200

    added item register added golden coin added localisation file

--- src/main/java/com/fabiii/kingdoms/item/ModItems.java
+++ src/main/java/com/fabiii/kingdoms/item/ModItems.java
@@ -1,10 +1,20 @@
 package com.fabiii.kingdoms.item;
 
 import com.fabiii.kingdoms.Kingdoms;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Kingdoms.MOD_ID);
+
+    public static final RegistryObject<Item> GOLDEN_COIN = ITEMS.register("golden_coin", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
